ec772dca95c7ff367cc1be30513cb9de1a3f8317
Panuwat IINC <panuwat.ja.forwork@gmail.com>
2025-06-29 23:59:47 +0700

    update

--- components/other/Header.jsx
+++ components/other/Header.jsx
@@ -7,7 +7,7 @@ export default function Header({ Header }) {
   return (
     <>
       <div className="flex flex-row items-center justify-center w-full p-2 gap-2">
-        <div className="flex items-center justify-start w-full h-full p-2 gap-2 font-[600]">
+        <div className="flex items-center justify-start w-full h-full p-2 gap-2 text-xl font-[600]">
           <Home /> {Header}
         </div>
       </div>
